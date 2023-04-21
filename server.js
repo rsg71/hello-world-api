7466f08ce0735dace06780860d331065cee7bcf3
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-04-20 20:57:37 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3055;
+
+app.get('/', (req, res) => {
+    let date = new Date().toLocaleDateString();
+    let time = new Date().toLocaleTimeString();
+    res.send(`You are connected on ${date} at ${time}`)
+})
+
+app.get('/api/test', (req, res) => {
+    let date = new Date().toLocaleDateString();
+    let time = new Date().toLocaleTimeString();
+    res.send(`Health check status: ✔ working on ${date} at ${time}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`)
+})
